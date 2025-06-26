644f90ce75cba8b4d26b8be3e6bb42ae3213a885
Jordy Slagter <slagterj2001@gmail.com>
2025-06-26 20:24:56 +0200

    finish customer functionality

--- src/pages/customer.tsx
+++ src/pages/customer.tsx
@@ -1,12 +1,117 @@
 ﻿import { FC, useEffect, useState } from "react";
 import { Content } from "antd/lib/layout/layout";
-import { Button, Dropdown, MenuProps, Typography } from "antd";
+import {
+  Button,
+  Divider,
+  Dropdown,
+  Form,
+  InputNumber,
+  MenuProps,
+  Typography,
+} from "antd";
 import { supabase } from "../global/initSupabase";
 
 interface Customer {
   id: string;
   name: string;
 }
+
+interface Product {
+  id: string;
+  productName: string;
+}
+
+interface OrderFormProps {
+  customerId: string;
+}
+
+const OrderForm: FC<OrderFormProps> = ({ customerId }) => {
+  const [form] = Form.useForm();
+  const [products, setProducts] = useState<Product[]>([]);
+  const [selectedProductId, setSelectedProductId] = useState<string | null>(
+    null,
+  );
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await supabase.from("Products").select();
+
+      if (error) {
+        console.error("Failed to load products", error);
+        return;
+      }
+
+      // Store full customer objects
+      setProducts(data as Product[]);
+    })();
+  }, []);
+
+  const handleMenuClick: MenuProps["onClick"] = (info) => {
+    setSelectedProductId(info.key);
+    form.setFieldsValue({ product: info.key });
+  };
+
+  const menuItems: MenuProps["items"] = products.map((product) => ({
+    key: product.id, // this is the customer ID
+    label: product.productName,
+  }));
+
+  const selectedProduct = products.find(
+    (product) => product.id === selectedProductId,
+  );
+
+  const handleSubmit = (values: any) => {
+    (async () => {
+      const { error } = await supabase.from("Orders").insert([
+        {
+          customerId: customerId,
+          productId: values.product,
+          productQuantity: values.quantity,
+        },
+      ]);
+
+      error && console.error(error);
+
+      form.resetFields();
+      setSelectedProductId(null);
+    })();
+  };
+
+  return (
+    <Form form={form} onFinish={handleSubmit}>
+      <Form.Item
+        label="Product"
+        name="product"
+        rules={[{ required: true, message: "Vul a.u.b. een product in" }]}
+      >
+        <Dropdown
+          menu={{
+            items: menuItems,
+            onClick: handleMenuClick,
+          }}
+        >
+          <Button>
+            {selectedProduct
+              ? selectedProduct.productName
+              : "Kies uw gewenste product ↓"}
+          </Button>
+        </Dropdown>
+      </Form.Item>
+      <Form.Item
+        label="Aantal"
+        name="quantity"
+        rules={[{ required: true, message: "Vul a.u.b. het aantal in" }]}
+      >
+        <InputNumber min={1} max={3} />
+      </Form.Item>
+
+      <Form.Item label={null}>
+        <Button type="primary" htmlType="submit">
+          Verzenden
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 
 const CustomerPage: FC = () => {
   const [customers, setCustomers] = useState<Customer[]>([]);
@@ -41,24 +146,32 @@ const CustomerPage: FC = () => {
   );
 
   return (
-    <Content style={{ padding: 24 }}>
-      <Dropdown
-        menu={{
-          items: menuItems,
-          onClick: handleMenuClick,
-        }}
-      >
-        <Button>
-          {selectedCustomer ? selectedCustomer.name : "Kies uw naam ↓"}
-        </Button>
-      </Dropdown>
+    <>
+      <Content style={{ padding: 24 }}>
+        <Dropdown
+          menu={{
+            items: menuItems,
+            onClick: handleMenuClick,
+          }}
+        >
+          <Button>
+            {selectedCustomer ? selectedCustomer.name : "Kies uw naam ↓"}
+          </Button>
+        </Dropdown>
 
+        {selectedCustomerId && (
+          <Typography style={{ marginTop: 16 }}>
+            Geselecteerde klant ID: <strong>{selectedCustomerId}</strong>
+          </Typography>
+        )}
+      </Content>
       {selectedCustomerId && (
-        <Typography style={{ marginTop: 16 }}>
-          Geselecteerde klant ID: <strong>{selectedCustomerId}</strong>
-        </Typography>
+        <>
+          <Divider />
+          <OrderForm customerId={selectedCustomerId} />
+        </>
       )}
-    </Content>
+    </>
   );
 };
 

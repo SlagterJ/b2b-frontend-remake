27ac3d595dca1e8982550f3ecd8dc173d227ca11
Geert Bruls <geertbruls@outlook.com>
2025-06-26 20:43:26 +0200

    accountmanager part 1

--- src/pages/accountmanagement.tsx
+++ src/pages/accountmanagement.tsx
@@ -1,11 +1,68 @@
-﻿import { FC } from "react";
-import { Space, Typography } from "antd";
+﻿import { FC, useEffect, useState } from "react";
+import { Space, Typography, List, Collapse, Table } from "antd";
+import { supabase } from "../global/initSupabase";
+import { Content } from "antd/lib/layout/layout";
 
 const AccountManagementPage: FC = () => {
+  const [orders, setOrders] = useState<any[] | undefined>(undefined);
+  useEffect(() => {
+    (async () => {
+      const { data } = await supabase.from("Orders").select();
+      const orderData: any[] = [];
+
+      const { data: customersData, error: customersError } = await supabase
+        .from("Customers")
+        .select("id, name");
+      const customerMap = new Map(customersData!.map((c) => [c.id, c.name]));
+
+      const { data: productsData, error: productsError } = await supabase
+        .from("Products")
+        .select("id, productName");
+      const productMap = new Map(
+        productsData!.map((c) => [c.id, c.productName]),
+      );
+
+      data!.forEach((item) => {
+        orderData.push({
+          key: item.id,
+          quantity: item.productQuantity,
+          orderPediod: item.orderPeriod,
+          customerName: customerMap.get(item.customerId) ?? "Unknown",
+          productName: productMap.get(item.productId) ?? "Unknown",
+        });
+      });
+
+      setOrders(orderData);
+    })();
+  }, []);
+
+  const columns = [
+    {
+      title: "Klantnaam",
+      dataIndex: "customerName",
+      key: "customerName",
+    },
+    {
+      title: "Product",
+      dataIndex: "productName",
+      key: "productName",
+    },
+    {
+      title: "Aantal",
+      dataIndex: "quantity",
+      key: "quantity",
+    },
+    {
+      title: "Orderperiode",
+      dataIndex: "orderPeriod",
+      key: "orderPeriod",
+    },
+  ];
+
   return (
-    <Space>
-      <Typography>Hello, account management!</Typography>
-    </Space>
+    <Content>
+      <Table dataSource={orders} columns={columns} />;
+    </Content>
   );
 };
 

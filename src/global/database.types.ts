40add692fa0b3adb1bf732873175cbd894df0e29
Jordy Slagter <slagterj2001@gmail.com>
2025-06-26 14:57:21 +0200

    add customer choice screen

--- src/global/database.types.ts
+++ src/global/database.types.ts
@@ -4,212 +4,212 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export type Database = {
   public: {
     Tables: {
       Customers: {
         Row: {
-          createdAt: string
-          id: string
-          name: string | null
-        }
+          createdAt: string;
+          id: string;
+          name: string | null;
+        };
         Insert: {
-          createdAt?: string
-          id?: string
-          name?: string | null
-        }
+          createdAt?: string;
+          id?: string;
+          name?: string | null;
+        };
         Update: {
-          createdAt?: string
-          id?: string
-          name?: string | null
-        }
-        Relationships: []
-      }
+          createdAt?: string;
+          id?: string;
+          name?: string | null;
+        };
+        Relationships: [];
+      };
       Orders: {
         Row: {
-          approvalPeriod: number | null
-          createdAt: string
-          customerId: string
-          deliveredPeriod: number | null
-          id: string
-          orderPeriod: number | null
-          productId: string
-          productQuantity: number
-          rejectedPeriod: number | null
-          status: Database["public"]["Enums"]["orderStatus"] | null
-        }
+          approvalPeriod: number | null;
+          createdAt: string;
+          customerId: string;
+          deliveredPeriod: number | null;
+          id: string;
+          orderPeriod: number | null;
+          productId: string;
+          productQuantity: number;
+          rejectedPeriod: number | null;
+          status: Database["public"]["Enums"]["orderStatus"] | null;
+        };
         Insert: {
-          approvalPeriod?: number | null
-          createdAt?: string
-          customerId: string
-          deliveredPeriod?: number | null
-          id?: string
-          orderPeriod?: number | null
-          productId: string
-          productQuantity: number
-          rejectedPeriod?: number | null
-          status?: Database["public"]["Enums"]["orderStatus"] | null
-        }
+          approvalPeriod?: number | null;
+          createdAt?: string;
+          customerId: string;
+          deliveredPeriod?: number | null;
+          id?: string;
+          orderPeriod?: number | null;
+          productId: string;
+          productQuantity: number;
+          rejectedPeriod?: number | null;
+          status?: Database["public"]["Enums"]["orderStatus"] | null;
+        };
         Update: {
-          approvalPeriod?: number | null
-          createdAt?: string
-          customerId?: string
-          deliveredPeriod?: number | null
-          id?: string
-          orderPeriod?: number | null
-          productId?: string
-          productQuantity?: number
-          rejectedPeriod?: number | null
-          status?: Database["public"]["Enums"]["orderStatus"] | null
-        }
+          approvalPeriod?: number | null;
+          createdAt?: string;
+          customerId?: string;
+          deliveredPeriod?: number | null;
+          id?: string;
+          orderPeriod?: number | null;
+          productId?: string;
+          productQuantity?: number;
+          rejectedPeriod?: number | null;
+          status?: Database["public"]["Enums"]["orderStatus"] | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "Orders_customerId_fkey"
-            columns: ["customerId"]
-            isOneToOne: false
-            referencedRelation: "Customers"
-            referencedColumns: ["id"]
+            foreignKeyName: "Orders_customerId_fkey";
+            columns: ["customerId"];
+            isOneToOne: false;
+            referencedRelation: "Customers";
+            referencedColumns: ["id"];
           },
           {
-            foreignKeyName: "Orders_productId_fkey"
-            columns: ["productId"]
-            isOneToOne: false
-            referencedRelation: "Products"
-            referencedColumns: ["id"]
+            foreignKeyName: "Orders_productId_fkey";
+            columns: ["productId"];
+            isOneToOne: false;
+            referencedRelation: "Products";
+            referencedColumns: ["id"];
           },
-        ]
-      }
+        ];
+      };
       ProductionLines: {
         Row: {
-          createdAt: string
-          id: string
-          name: string | null
-        }
+          createdAt: string;
+          id: string;
+          name: string | null;
+        };
         Insert: {
-          createdAt?: string
-          id?: string
-          name?: string | null
-        }
+          createdAt?: string;
+          id?: string;
+          name?: string | null;
+        };
         Update: {
-          createdAt?: string
-          id?: string
-          name?: string | null
-        }
-        Relationships: []
-      }
+          createdAt?: string;
+          id?: string;
+          name?: string | null;
+        };
+        Relationships: [];
+      };
       Products: {
         Row: {
-          blueBlocks: number | null
-          createdAt: string
-          greyBlocks: number | null
-          id: string
-          price: number | null
-          productionSeconds: number | null
-          productName: string | null
-          redBlocks: number | null
-        }
+          blueBlocks: number | null;
+          createdAt: string;
+          greyBlocks: number | null;
+          id: string;
+          price: number | null;
+          productionSeconds: number | null;
+          productName: string | null;
+          redBlocks: number | null;
+        };
         Insert: {
-          blueBlocks?: number | null
-          createdAt?: string
-          greyBlocks?: number | null
-          id?: string
-          price?: number | null
-          productionSeconds?: number | null
-          productName?: string | null
-          redBlocks?: number | null
-        }
+          blueBlocks?: number | null;
+          createdAt?: string;
+          greyBlocks?: number | null;
+          id?: string;
+          price?: number | null;
+          productionSeconds?: number | null;
+          productName?: string | null;
+          redBlocks?: number | null;
+        };
         Update: {
-          blueBlocks?: number | null
-          createdAt?: string
-          greyBlocks?: number | null
-          id?: string
-          price?: number | null
-          productionSeconds?: number | null
-          productName?: string | null
-          redBlocks?: number | null
-        }
-        Relationships: []
-      }
+          blueBlocks?: number | null;
+          createdAt?: string;
+          greyBlocks?: number | null;
+          id?: string;
+          price?: number | null;
+          productionSeconds?: number | null;
+          productName?: string | null;
+          redBlocks?: number | null;
+        };
+        Relationships: [];
+      };
       PurchaseOrders: {
         Row: {
-          createdAt: string
-          deliveredPeriod: number | null
-          id: string
-          orderPeriod: number | null
-          workOderId: string | null
-        }
+          createdAt: string;
+          deliveredPeriod: number | null;
+          id: string;
+          orderPeriod: number | null;
+          workOderId: string | null;
+        };
         Insert: {
-          createdAt?: string
-          deliveredPeriod?: number | null
-          id?: string
-          orderPeriod?: number | null
-          workOderId?: string | null
-        }
+          createdAt?: string;
+          deliveredPeriod?: number | null;
+          id?: string;
+          orderPeriod?: number | null;
+          workOderId?: string | null;
+        };
         Update: {
-          createdAt?: string
-          deliveredPeriod?: number | null
-          id?: string
-          orderPeriod?: number | null
-          workOderId?: string | null
-        }
+          createdAt?: string;
+          deliveredPeriod?: number | null;
+          id?: string;
+          orderPeriod?: number | null;
+          workOderId?: string | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "PurchaseOrders_workOderId_fkey"
-            columns: ["workOderId"]
-            isOneToOne: false
-            referencedRelation: "WorkOrder"
-            referencedColumns: ["id"]
+            foreignKeyName: "PurchaseOrders_workOderId_fkey";
+            columns: ["workOderId"];
+            isOneToOne: false;
+            referencedRelation: "WorkOrder";
+            referencedColumns: ["id"];
           },
-        ]
-      }
+        ];
+      };
       WorkOrder: {
         Row: {
-          createdAt: string
-          id: string
-          orderId: string
-          planedPeriod: number | null
-          productionLineId: string | null
-        }
+          createdAt: string;
+          id: string;
+          orderId: string;
+          planedPeriod: number | null;
+          productionLineId: string | null;
+        };
         Insert: {
-          createdAt?: string
-          id?: string
-          orderId: string
-          planedPeriod?: number | null
-          productionLineId?: string | null
-        }
+          createdAt?: string;
+          id?: string;
+          orderId: string;
+          planedPeriod?: number | null;
+          productionLineId?: string | null;
+        };
         Update: {
-          createdAt?: string
-          id?: string
-          orderId?: string
-          planedPeriod?: number | null
-          productionLineId?: string | null
-        }
+          createdAt?: string;
+          id?: string;
+          orderId?: string;
+          planedPeriod?: number | null;
+          productionLineId?: string | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "WorkOrder_orderId_fkey"
-            columns: ["orderId"]
-            isOneToOne: false
-            referencedRelation: "Orders"
-            referencedColumns: ["id"]
+            foreignKeyName: "WorkOrder_orderId_fkey";
+            columns: ["orderId"];
+            isOneToOne: false;
+            referencedRelation: "Orders";
+            referencedColumns: ["id"];
           },
           {
-            foreignKeyName: "WorkOrder_productionLineId_fkey"
-            columns: ["productionLineId"]
-            isOneToOne: false
-            referencedRelation: "ProductionLines"
-            referencedColumns: ["id"]
+            foreignKeyName: "WorkOrder_productionLineId_fkey";
+            columns: ["productionLineId"];
+            isOneToOne: false;
+            referencedRelation: "ProductionLines";
+            referencedColumns: ["id"];
           },
-        ]
-      }
-    }
+        ];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
       orderStatus:
         | "InProduction"
@@ -218,22 +218,22 @@ export type Database = {
         | "Delivered"
         | "Rejected"
         | "PendingApproval"
-        | "Planning"
-    }
+        | "Planning";
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type DefaultSchema = Database[Extract<keyof Database, "public">]
+type DefaultSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
     | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
         Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
@@ -241,7 +241,7 @@ export type Tables<
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
   ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
       Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+      Row: infer R;
     }
     ? R
     : never
@@ -249,64 +249,64 @@ export type Tables<
         DefaultSchema["Views"])
     ? (DefaultSchema["Tables"] &
         DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
-        Row: infer R
+        Row: infer R;
       }
       ? R
       : never
-    : never
+    : never;
 
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
   ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+      Insert: infer I;
     }
     ? I
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
     ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Insert: infer I
+        Insert: infer I;
       }
       ? I
       : never
-    : never
+    : never;
 
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
   ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+      Update: infer U;
     }
     ? U
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
     ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Update: infer U
+        Update: infer U;
       }
       ? U
       : never
-    : never
+    : never;
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
     | keyof DefaultSchema["Enums"]
     | { schema: keyof Database },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
@@ -314,14 +314,14 @@ export type Enums<
   ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
     ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-    : never
+    : never;
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof DefaultSchema["CompositeTypes"]
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
@@ -329,7 +329,7 @@ export type CompositeTypes<
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
     ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
+    : never;
 
 export const Constants = {
   public: {
@@ -345,4 +345,4 @@ export const Constants = {
       ],
     },
   },
-} as const
+} as const;
--- src/pages/customer.tsx
+++ src/pages/customer.tsx
@@ -1,11 +1,29 @@
-﻿import { FC } from "react";
+﻿import { FC, useEffect, useState } from "react";
 import { Content } from "antd/lib/layout/layout";
-import { Typography } from "antd";
+import { Dropdown } from "antd";
+import { supabase } from "../global/initSupabase";
 
 const CustomerPage: FC = () => {
+  const [customers, setCustomers] = useState<any[] | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await supabase.from("Customers").select();
+      const customerData: any[] = [];
+
+      data!.forEach((item) => {
+        customerData.push({ key: item.id, label: item.name });
+      });
+
+      setCustomers(customerData);
+    })();
+  }, []);
+
   return (
     <Content>
-      <Typography>Hello, customer!</Typography>
+      {customers && (
+        <Dropdown menu={{ items: customers }}>Kies uw naam</Dropdown>
+      )}
     </Content>
   );
 };

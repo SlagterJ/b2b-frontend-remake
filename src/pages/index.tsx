784db429ccb371ee97dda479d09f5f326baaf466
Jordy Slagter <slagterj2001@gmail.com>
2025-06-27 10:07:39 +0200

    add statistics, reformat pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,108 @@
-﻿import { FC } from "react";
-import { Space, Typography } from "antd";
+﻿import { FC, useEffect, useState } from "react";
+import { Col, Row, Space, Statistic, Typography } from "antd";
+import { Content } from "antd/es/layout/layout";
+import { supabase } from "../global/initSupabase";
+
+interface Order {
+  productQuantity: number;
+  Products: {
+    blueBlocks: number;
+    redBlocks: number;
+    greyBlocks: number;
+  };
+}
 
 const HomePage: FC = () => {
+  const [totalOrders, setTotalOrders] = useState(0);
+  const [totalOrdersFinished, setTotalOrdersFinished] = useState(0);
+  const [totalBlocks, setTotalBlocks] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      const { count: totalOrdersCount, error: totalOrdersError } =
+        await supabase
+          .from("Orders")
+          .select("*", { count: "exact", head: true });
+
+      if (totalOrdersError) {
+        console.error(
+          "There was an error getting the total amount of orders",
+          totalOrdersError,
+        );
+        return;
+      }
+
+      setTotalOrders(totalOrdersCount as number);
+
+      const {
+        count: totalFinishedOrdersCount,
+        error: totalFinishedOrdersError,
+      } = await supabase
+        .from("Orders")
+        .select("*", { count: "exact", head: true })
+        .eq("status", "Delivered");
+
+      if (totalFinishedOrdersError) {
+        console.error(
+          "There was an error getting the total amount of finished orders",
+          totalFinishedOrdersError,
+        );
+        return;
+      }
+
+      setTotalOrdersFinished(totalFinishedOrdersCount as number);
+
+      const { data: totalBlocks, error: totalBlocksError } = await supabase
+        .from("Orders")
+        .select("*, Products(*)")
+        .eq("status", "Delivered");
+
+      if (totalBlocksError) {
+        console.error(
+          "There was an error getting the total amount of blocks",
+          totalBlocksError,
+        );
+        return;
+      }
+
+      const totals = (totalBlocks as Order[]).reduce(
+        (acc, order) => {
+          acc.blue +=
+            (order.productQuantity || 0) * (order.Products?.blueBlocks || 0);
+          acc.red +=
+            (order.productQuantity || 0) * (order.Products?.redBlocks || 0);
+          acc.grey +=
+            (order.productQuantity || 0) * (order.Products?.greyBlocks || 0);
+          return acc;
+        },
+        { blue: 0, red: 0, grey: 0 },
+      );
+
+      setTotalBlocks(totals.blue + totals.red + totals.grey);
+    })();
+  }, []);
+
   return (
-    <Space>
-      <Typography>Hello, World!</Typography>
-    </Space>
+    <Content>
+      <Typography.Title>Dashboard</Typography.Title>
+      <Row>
+        <Col span={6}>
+          <Statistic title="Totaal aantal orders" value={totalOrders} />
+        </Col>
+        <Col span={6}>
+          <Statistic
+            title="Aantal orders succesvol geleverd"
+            value={totalOrdersFinished}
+          />
+        </Col>
+        <Col span={6}>
+          <Statistic
+            title="Aantal blokjes geleverd totaal"
+            value={totalBlocks}
+          />
+        </Col>
+      </Row>
+    </Content>
   );
 };
 

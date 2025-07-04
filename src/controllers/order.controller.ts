46809f801b2f087696fe96c8fa2430539f182f6b
Jordy Slagter <slagterj2001@gmail.com>
2025-07-04 14:06:45 +0200

    update order model

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,6 +1,12 @@
 ﻿import { Order } from "../models/order.model";
 import { createBackendRoute } from "../global/env";
 
+export interface CreateOrderProperties {
+  customerId: number;
+  productId: number;
+  status: string;
+}
+
 export class OrderController {
   public static async readAll(): Promise<Order[]> {
     const response = await fetch(createBackendRoute("Orders"));
@@ -14,15 +20,15 @@ export class OrderController {
     return data.map((item: any) => Order.fromJSON(item));
   }
 
-  public static async create(order: Order): Promise<Order> {
-    const orderJSON = order.toJSON();
-
-    await fetch(createBackendRoute("Orders"), {
+  public static async create(order: CreateOrderProperties): Promise<Order> {
+    const response = await fetch(createBackendRoute("Orders"), {
       method: "POST",
-      body: JSON.stringify(orderJSON),
+      body: JSON.stringify(order),
     });
 
-    return order;
+    const data = await response.json();
+
+    return Order.fromJSON(data);
   }
 
   public static async update(order: Order): Promise<Order> {
@@ -38,12 +44,13 @@ export class OrderController {
     return order;
   }
 
-  public static async delete(order: Order): Promise<Order> {
-    const id = order.id;
+  public static async delete(id: number): Promise<boolean> {
     await fetch(createBackendRoute(["Orders", id.toString()]), {
       method: "DELETE",
+    }).catch((_) => {
+      return false;
     });
 
-    return order;
+    return true;
   }
 }

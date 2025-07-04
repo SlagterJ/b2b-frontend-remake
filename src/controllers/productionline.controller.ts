81927424da511821d9ddf43f73b83b80b772531a
Geert Bruls <geertbruls@outlook.com>
2025-07-04 12:36:56 +0200

    Added productionline.controller

---
+++ src/controllers/productionline.controller.ts
@@ -0,0 +1,57 @@
+import { ProductionLine } from "../models/productionline.model";
+import { createBackendRoute } from "../global/env";
+
+export class ProductionLineController {
+  public static async readAll(): Promise<ProductionLine[]> {
+    const response = await fetch(createBackendRoute("ProductionLine"));
+    const data = await response.json();
+    return data.map((item: any) => ProductionLine.fromJSON(item));
+  }
+
+  public static async readOneById(id: number): Promise<ProductionLine> {
+    const response = await fetch(
+      createBackendRoute(["ProductionLine", id.toString()]),
+    );
+    const data = await response.json();
+    return data.map((item: any) => ProductionLine.fromJSON(item));
+  }
+
+  public static async create(
+    productionLine: ProductionLine,
+  ): Promise<ProductionLine> {
+    const customerJSON = productionLine.toJSON();
+
+    await fetch(createBackendRoute("ProductionLine"), {
+      method: "POST",
+      body: JSON.stringify(customerJSON),
+    });
+
+    return productionLine;
+  }
+
+  public static async update(
+    productionLine: ProductionLine,
+  ): Promise<ProductionLine> {
+    const id = productionLine.id;
+
+    const customerJSON = productionLine.toJSON();
+
+    await fetch(createBackendRoute(["ProductionLine", id.toString()]), {
+      method: "PUT",
+      body: JSON.stringify(customerJSON),
+    });
+
+    return productionLine;
+  }
+
+  public static async delete(
+    productionLine: ProductionLine,
+  ): Promise<ProductionLine> {
+    const id = productionLine.id;
+    await fetch(createBackendRoute(["ProductionLine", id.toString()]), {
+      method: "DELETE",
+    });
+
+    return productionLine;
+  }
+}

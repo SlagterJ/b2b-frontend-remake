2fe0b6a4567556666b446cf89a778494c81f6879
Jordy Slagter <slagterj2001@gmail.com>
2025-07-04 12:24:33 +0200

    add customer controller

---
+++ src/controllers/customer.controller.ts
@@ -0,0 +1,51 @@
+﻿import { Customer } from "../models/customer.model";
+import { createBackendRoute } from "../global/env";
+
+export class CustomerController {
+  static async readAll(): Promise<Customer[]> {
+    const response = await fetch(createBackendRoute("Customers"));
+    const data = await response.json();
+    return data.map((item: any) => Customer.fromJSON(item));
+  }
+
+  static async readOneById(id: number): Promise<Customer> {
+    const response = await fetch(
+      createBackendRoute(["Customers", id.toString()]),
+    );
+    const data = await response.json();
+    return data.map((item: any) => Customer.fromJSON(item));
+  }
+
+  static async create(customer: Customer): Promise<Customer> {
+    const customerJSON = customer.toJSON();
+
+    await fetch(createBackendRoute("Customers"), {
+      method: "POST",
+      body: JSON.stringify(customerJSON),
+    });
+
+    return customer;
+  }
+
+  static async update(customer: Customer): Promise<Customer> {
+    const id = customer.id;
+
+    const customerJSON = customer.toJSON();
+
+    await fetch(createBackendRoute(["Customers", id.toString()]), {
+      method: "PUT",
+      body: JSON.stringify(customerJSON),
+    });
+
+    return customer;
+  }
+
+  static async delete(customer: Customer): Promise<Customer> {
+    const id = customer.id;
+    await fetch(createBackendRoute(["Customers", id.toString()]), {
+      method: "DELETE",
+    });
+
+    return customer;
+  }
+}
